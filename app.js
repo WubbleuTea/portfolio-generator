318f04a17c8f3cfaa773643718a5e91a483f6bf2
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-07-13 17:29:33 -0500

    tested inquirer

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer')
 const profileDataArgs = process.argv.slice(2)
 const [name, github] = profileDataArgs;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
 
-    console.log('portfolio complete! checkout index.html to see the output!')
-});
+//     console.log('portfolio complete! checkout index.html to see the output!')
+// });
 
