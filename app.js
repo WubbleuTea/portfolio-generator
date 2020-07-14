4a7c427bbd527a3a6dc1ab22ca63b00de7801a05
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-07-14 18:51:28 -0500

    html now populates projects

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js')
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
 const inquirer = require('inquirer')
 const profileDataArgs = process.argv.slice(2)
+
 const [name, github] = profileDataArgs;
 
 const promptUser = () => {
@@ -130,8 +131,15 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData)
+        const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
     });
+  });
+
     
 
 // fs.writeFile('./index.html', generatePage(name, github), err => {
